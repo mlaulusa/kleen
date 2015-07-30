3994b451ef5d64d2f26a6b727633e9f7d4acff0c
Matthew <mlaulusa@users.noreply.github.com>
2015-07-29 18:29:16 -0600

    Added the an angular client factory

--- app/ngComponents/factories/ClientFactory.js
+++ app/ngComponents/factories/ClientFactory.js
@@ -1,18 +1,17 @@
-
 // This factory will return a function whereby you may call it passing in the client _id
 // and it will return the client object associated with that _id
 
-function Client($http, $log, client_id){
-  this.client = function(callback){
+function Client($http, $log, client_id) {
+  this.client = function(callback) {
     $http.get('/api/client/' + client_id)
-      .success(function(value){
+      .success(function(value) {
         callback(value);
       });
   };
 };
 
-angular.factory('OneClientFactory', ['$http', '$log', function($http, $log){
-  return function(client_id){
+angular.factory('ClientFactory', ['$http', '$log', function($http, $log) {
+  return function(client_id) {
     return new Client($http, $log, client_id);
   };
 }]);
